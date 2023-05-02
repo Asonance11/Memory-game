e34573677692faef4f69fec263d951552ec3ca44
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-02 16:25:18 +0100

    addition of images

---
+++ src/data/images.js
@@ -0,0 +1,33 @@
+const luffy =
+	'https://static.wikia.nocookie.net/onepiece/images/6/6d/Monkey_D._Luffy_Anime_Post_Timeskip_Infobox.png';
+const zoro =
+	'https://static.wikia.nocookie.net/onepiece/images/5/52/Roronoa_Zoro_Anime_Post_Timeskip_Infobox.png';
+const nami =
+	'https://static.wikia.nocookie.net/onepiece/images/6/68/Nami_Anime_Post_Timeskip_Infobox.png';
+const ussop =
+	'https://static.wikia.nocookie.net/onepiece/images/3/35/Usopp_Anime_Post_Timeskip_Infobox.png';
+const sanji =
+	'https://static.wikia.nocookie.net/onepiece/images/b/b6/Sanji_Anime_Post_Timeskip_Infobox.png';
+const chopper =
+	'https://static.wikia.nocookie.net/onepiece/images/a/af/Tony_Tony_Chopper_Anime_Post_Timeskip_Infobox.png';
+const robin =
+	'https://static.wikia.nocookie.net/onepiece/images/b/bc/Nico_Robin_Anime_Post_Timeskip_Infobox.png';
+const franky =
+	'https://static.wikia.nocookie.net/onepiece/images/8/8c/Franky_Anime_Post_Timeskip_Infobox.png';
+const brook =
+	'https://static.wikia.nocookie.net/onepiece/images/4/41/Brook_Anime_Post_Timeskip_Infobox.png';
+const jinbei =
+	'https://static.wikia.nocookie.net/onepiece/images/8/81/Jinbe_Anime_Infobox.png';
+
+export {
+	luffy,
+	zoro,
+	nami,
+	ussop,
+	sanji,
+	chopper,
+	robin,
+	franky,
+	brook,
+	jinbei,
+};
