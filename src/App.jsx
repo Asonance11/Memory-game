1fbc0a64780ead5f13be24975773a8ae5ede8535
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-02 16:36:33 +0100

    addition of functions that affect score

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 
 const App = () => {
+	const [score, setScore] = useState(0);
+	const [highScore, setHighScore] = useState(0);
+
+	const incrementScore = () => {
+		setScore(score + 1);
+	};
+
+	const incremetHighScore = () => {
+		if (score > highScore) {
+			setHighScore(score + 1);
+		}
+	};
+
+	const clearScore = () => {
+		setScore(0);
+	};
+
 	return (
 		<div>
-			<Nav />
+			<Nav score={score} highScore={highScore} />
 		</div>
 	);
 };
