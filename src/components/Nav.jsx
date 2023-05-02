6d5351632c4fb882dc54a873cc95c3c2d32b4fe2
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-02 16:37:33 +0100

    addition of score values as props

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Nav = () => {
+const Nav = (props) => {
 	return (
 		<header className="p-4 bg-slate-950 flex items-center justify-between text-white">
 			<h1 className="text-3xl font-bold">Memory Game</h1>
 			<ul>
-				<li>Score: 0</li>
-				<li>Best Score: 0</li>
+				<li>Score: {props.score}</li>
+				<li>Best Score: {props.highScore}</li>
 			</ul>
 		</header>
 	);
