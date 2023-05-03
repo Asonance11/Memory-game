e03844dbc223b673321cfd19fb4ddf9b0c898b8f
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-03 10:04:01 +0100

    Creation of images state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+	brook,
+	chopper,
+	franky,
+	jinbei,
+	luffy,
+	nami,
+	robin,
+	sanji,
+	ussop,
+	zoro,
+} from '../data/images';
 
 const Main = ({ incrementScore, incrementHighScore, clearScore }) => {
+	const [images, setImages] = useState([
+		luffy,
+		zoro,
+		nami,
+		ussop,
+		sanji,
+		chopper,
+		robin,
+		franky,
+		brook,
+		jinbei,
+	]);
+
 	return <div>Main</div>;
 };
 
